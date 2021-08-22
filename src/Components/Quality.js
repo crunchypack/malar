69d3932bd8327e7312b99c2c0e48258509ffdc5b
Crunchypack <simon.wolf.lobo@gmail.com>
2021-08-22 16:24:27 -0400

    Quality

---
+++ src/Components/Quality.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Quality extends React.Component{
+    render(){
+        return(
+            <React.Fragment>
+                <div>
+                    <div>
+                        <img src='' />
+                    </div>
+                    <div>
+                        <img src='' />
+                    </div>
+                </div>
+            </React.Fragment>
+        )
+    }
+}
