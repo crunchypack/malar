901b9a7cd24d4a4c2accea8cc6bf73db6dd293c2
Andres-1992 <Andresloboroca@gmail.com>
2021-09-07 20:27:00 +0200

    shiny package

--- src/Components/ServiceSection.js
+++ src/Components/ServiceSection.js
@@ -1,24 +1,24 @@
-import  Button  from 'react-bootstrap/Button';
-import React from 'react';
-import '../App.css';
+// import  Button  from 'react-bootstrap/Button';
+// import React from 'react';
+// import '../App.css';
 
-import './ServiceSection.css';
-import { Link } from 'react-router-dom';
-function ServiceSection() {
-  return (
-    <div className='service-container1'>
+// import './ServiceSection.css';
+// import { Link } from 'react-router-dom';
+// function ServiceSection() {
+//   return (
+//     <div className='service-container1'>
 
-      <p>Behöver du rådgivning?</p>
+//       <p>Behöver du rådgivning?</p>
 
-      <p>Kontakta oss för ett kostnadsfritt hembesök</p>
-      <div className='service-btns1' >
-        <Link to='/kontakta-oss'><Button variant='outline-light'>
-          Kontakta oss
-        </Button></Link>
-      </div>
-    </div>
-  );
-}
+//       <p>Kontakta oss för ett kostnadsfritt hembesök</p>
+//       <div className='service-btns1' >
+//         <Link to='/kontakta-oss'><Button variant='outline-light'>
+//           Kontakta oss
+//         </Button></Link>
+//       </div>
+//     </div>
+//   );
+// }
 
-export default ServiceSection;
+// export default ServiceSection;
 
