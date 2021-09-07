e8a938c849e4a520dd3a0b8ac251d9e345141ae8
Crunchypack <simon.wolf.lobo@gmail.com>
2021-09-07 15:07:35 -0400

    sendgrid testad

--- src/Components/ContactForm.js
+++ src/Components/ContactForm.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+//const{SENDGRID_API_KEY} =require('../sendgrid');
 
-class ContactForm extends React.Component{
-  constructor(props){
+class ContactForm extends React.Component {
+  constructor(props) {
     super(props);
-    this.state ={
-      formName:"",
-      formEmail:"",
-      formPhone:"",
-      formMsg:"",
-      files:null
+    this.state = {
+      formName: "",
+      formEmail: "",
+      formPhone: "",
+      formMsg: "",
+      files: null,
     };
     this.formFiles = React.createRef();
     this.handleFormChange = this.handleFormChange.bind(this);
@@ -22,52 +23,107 @@ class ContactForm extends React.Component{
       [e.target.name]: e.target.value,
     });
   }
-  handleFile(e){
+  handleFile(e) {
     this.setState({
-      files:e.target.files
-    })
+      files: e.target.files,
+    });
   }
   handleFormSubmit(e) {
-    //Code Goes Here
-    e.preventDefault();
-    console.log(this.state);
+    // //Code Goes Here
+    // const sgMail = require("@sendgrid/mail");
+    // sgMail.setApiKey(SENDGRID_API_KEY);
+    // e.preventDefault();
+    // const msg = {
+    //   to: "simon.wolf.lobo@gmail.com", // Change to your recipient
+    //   from: "simon.lobo@hotmail.se", // Change to your verified sender
+    //   subject: "Kontaktformulär Målaresset",
+    //   text:
+    //   "From:" +
+    //   this.state.formName +
+    //   "\nEmail:" +
+    //   this.state.formEmail +
+    //   "\nTel.:" +
+    //   this.state.formPhone +
+    //   "\nMeddelande: " +
+    //   this.state.formMsg
+      
+    // };
+    // sgMail
+    //   .send(msg)
+    //   .then(() => {
+    //     console.log("Email sent");
+    //   })
+    //   .catch((error) => {
+    //     console.error(error);
+    //   });
 
     this.setState({
-      formName:"",
-      formEmail:"",
-      formPhone:"",
-      formMsg:"",
-      files:null
+      formName: "",
+      formEmail: "",
+      formPhone: "",
+      formMsg: "",
+      files: null,
     });
     this.formFiles.current.value = "";
   }
-    render(){
-        return(
-            <React.Fragment>
-                
-            <Form className="shadow-none" onSubmit={this.handleFormSubmit}>
-            <h2> Kontakta oss!</h2>
+  render() {
+    return (
+      <React.Fragment>
+        <Form className="shadow-none" onSubmit={this.handleFormSubmit}>
+          <h2> Kontakta oss!</h2>
 
-                <Form.Text className='text-muted'>
-                Att vara lätt tillgängliga är viktigt för att du ska känna förtroende för oss. Du når oss enklast direkt via telefon, mail eller genom att fylla i vårt formulär nedan. Vi lovar att återkomma till dig så snart vi kan!</Form.Text>
-              <Form.Group className="mb-3">
-                <Form.Label>Namn</Form.Label>
-                <Form.Control type="text" name="formName" value={this.state.formName} onChange={this.handleFormChange}/>
-                <Form.Label>E-mail</Form.Label>
-                <Form.Control type="email" name="formEmail" value={this.state.formEmail} onChange={this.handleFormChange} required/>
-                <Form.Label>Telefon</Form.Label>
-                <Form.Control type="tel" name="formPhone" value={this.state.formPhone} onChange={this.handleFormChange} required/>
-                <Form.Label>Meddelande</Form.Label>
-                <Form.Control as="textarea" rows={4} name="formMsg" value={this.state.formMsg} onChange={this.handleFormChange} />
-                <Form.Label>Bifoga bilder</Form.Label>
-                <Form.Control type="file" multiple onChange={this.handleFile} ref={this.formFiles}/>
-              </Form.Group>
-              <Button variant="success" type="submit" >
-                Submit
-              </Button>
-            </Form>
-          </React.Fragment>
-        )
-    }
+          <Form.Text className="text-muted">
+            Att vara lätt tillgängliga är viktigt för att du ska känna
+            förtroende för oss. Du når oss enklast direkt via telefon, mail
+            eller genom att fylla i vårt formulär nedan. Vi lovar att återkomma
+            till dig så snart vi kan!
+          </Form.Text>
+          <Form.Group className="mb-3">
+            <Form.Label>Namn</Form.Label>
+            <Form.Control
+              type="text"
+              name="formName"
+              value={this.state.formName}
+              onChange={this.handleFormChange}
+            />
+            <Form.Label>E-mail</Form.Label>
+            <Form.Control
+              type="email"
+              name="formEmail"
+              value={this.state.formEmail}
+              onChange={this.handleFormChange}
+              required
+            />
+            <Form.Label>Telefon</Form.Label>
+            <Form.Control
+              type="tel"
+              name="formPhone"
+              value={this.state.formPhone}
+              onChange={this.handleFormChange}
+              required
+            />
+            <Form.Label>Meddelande</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={4}
+              name="formMsg"
+              value={this.state.formMsg}
+              onChange={this.handleFormChange}
+            />
+            <Form.Label>Bifoga bilder</Form.Label>
+            <Form.Control
+              type="file"
+              multiple
+              onChange={this.handleFile}
+              ref={this.formFiles}
+            />
+          </Form.Group>
+          <Button variant="success" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </React.Fragment>
+    );
+  }
 }
 export default ContactForm;
